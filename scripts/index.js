88e827201c5bc79b4ee0c34a23caa2c801038f07
Matvey Nekrasov <matvey1234567@yandex.ru>
2024-01-04 22:42:10 +0300

    реализован заданный функционал

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
-// @todo: Темплейт карточки
+const cardsListContainer = document.querySelector(".places__list");
 
-// @todo: DOM узлы
+function createCard(card, deleteButtonClickHandler) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
 
-// @todo: Функция создания карточки
+  const cardImage = cardElement.querySelector(".card__image");
+  cardImage.src = card.link;
+  cardImage.alt = `Фотография ${card.name}`;
 
-// @todo: Функция удаления карточки
+  const cardTitle = cardElement.querySelector(".card__title");
+  cardTitle.textContent = card.name;
 
-// @todo: Вывести карточки на страницу
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", deleteButtonClickHandler);
+
+  return cardElement;
+}
+
+function deleteCard(evt) {
+  const clickedCard = evt.target.closest(".card");
+  clickedCard.remove();
+}
+
+initialCards.forEach((card) => {
+  cardsListContainer.append(createCard(card, deleteCard));
+});
